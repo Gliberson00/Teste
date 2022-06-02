48138b59f1f216d8ddb1be3ab1c4e742da927ca9
Glieberson <105432927+Gliberson00@users.noreply.github.com>
2022-06-02 14:00:58 -0300

    Add files via upload

---
+++ teste dia 02;06/script.js
@@ -0,0 +1,38 @@
+function calcular(n1,n2){
+    n1 = parseFloat(document.getElementById("n1").value)
+    n2 = parseFloat(document.getElementById("n2").value)
+
+    selector = document.getElementById("selector").value
+
+    switch(selector){
+        case'+':
+        calculo =(n1 + n2)
+        document.getElementById("resultado").innerHTML = `O resultado da soma de ${n1} + ${n2} e = ${calculo}`
+        break
+        
+        case'-':
+        calculo =(n1 - n2)
+        document.getElementById("resultado").innerHTML = `O resultado da subtração de ${n1} - ${n2} e = ${calculo}`
+        break
+
+        case'/':
+        calculo = math.round(n1 / n2)
+        if(Number.isNaN(cacular)){ document.getElementById("resultado").innerHTML = `insira um divisivel valido!`
+       }else{document.getElementById("resultado").innerHTML = `O resultado da divisãp de ${n1} - ${n2} e = ${calculo}`
+       }break
+    
+        case'*':
+        calculo =(n1 * n2)
+        document.getElementById("resultado").innerHTML = `O resultado da subtração de ${n1} * ${n2} é = ${calculo}.`
+        break
+        
+
+    
+
+        case'%':
+        calculo =(n1 * n2 ) / 100
+        document.getElementById("resultado").innerHTML = ` o resultatdo e ${calculo}`
+      break
+}
+
+}
